46bfdfc0e34cd283164edef19e506d79398ee064
Hassan Chamseddine <hassanchamseddine92@outlook.com>
2019-06-19 01:56:20 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import dayGridPlugin from '@fullcalendar/daygrid';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-full-calendar';
+  calendarPlugins = [dayGridPlugin];
+  calendarEvents = [
+    { title: 'event 1', date: '2019-04-01' }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular'; // for FullCalendar!
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FullCalendarModule,
     AppRoutingModule
   ],
   providers: [],
