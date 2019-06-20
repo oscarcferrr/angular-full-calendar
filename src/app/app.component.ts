07b94553a2e01121f31d5062f7388747149de92a
Hassan Chamseddine <hassanchamseddine92@outlook.com>
2019-06-20 12:44:08 +0200

    improving app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction'
+import { OptionsInput } from '@fullcalendar/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-full-calendar';
-  calendarPlugins = [dayGridPlugin];
-  calendarEvents = [
-    { title: 'event 1', date: '2019-04-01' }
-  ];
+  options: OptionsInput;
+  eventsModel: any;
+
+  @ViewChild('fullcalendar', { static: false }) fullcalendar: FullCalendarComponent;
+  ngOnInit() {
+    this.options = {
+      editable: true,
+      customButtons: {
+        myCustomButton: {
+          text: 'custom!',
+          click: function () {
+            alert('clicked the custom button!');
+          }
+        }
+      },
+      header: {
+        left: 'prev,next today myCustomButton',
+        center: 'title',
+        right: 'dayGridMonth'
+      },
+      plugins: [dayGridPlugin, interactionPlugin]
+    };
+
+  }
+  eventClick(model) {
+    console.log(model);
+  }
+  eventDragStop(model) {
+    console.log(model);
+  }
+  dateClick(model) {
+    console.log(model);
+  }
+  updateHeader() {
+    this.options.header = {
+      left: 'prev,next myCustomButton',
+      center: 'title',
+      right: ''
+    };
+  }
+  updateEvents() {
+    this.eventsModel = [{
+      title: 'Updaten Event',
+      start: this.yearMonth + '-08',
+      end: this.yearMonth + '-10'
+    }];
+  }
+  get yearMonth(): string {
+    const dateObj = new Date();
+    return dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
+  }
 }
